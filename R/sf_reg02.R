0f78abf18b985ecfeeb4755fc2d25bf59938d2a9
Sébastien Calvet <sebastien.calvet@insee.fr>
2019-08-30 15:02:14 +0200

    git commit -m

--- R/sf_reg02.R
+++ R/sf_reg02.R
@@ -2,7 +2,7 @@ sf_reg02 <-
 function()
 {
   # chemin du fond de carte .shp
-  path_to_shp <- system.file("extdata","reg00.shp", package = "oceanis")
+  path_to_shp <- system.file("extdata","reg02.shp", package = "oceanis")
   # import de l'objet sf
   reg02 <- st_read(dsn = path_to_shp, quiet = TRUE)
 return(reg02)
